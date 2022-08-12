10f076ce661178254391cf64531b5788ec29c073
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-12 15:13:02 +0300

    Решение 13ой задачи из списка ко второу семинару

---
+++ HomeWork_Exe13/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+if (number.Length==3)
+{
+    Console.WriteLine("Третья цифра числа:"+ number[2]);
+}
+
+else 
+if (number.Length<3)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
