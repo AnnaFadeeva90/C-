3749ee2265e3ae44900898b8ffefca99eceee4ad
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-12 14:49:25 +0300

    Решение задачи 10 ко второму семинару

---
+++ HomeWork_Exe10.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+if (number.Length==3)
+{
+    Console.WriteLine("Вторая цифра числа:"+ number[1]);
+}
+
+else
+{
+    Console.WriteLine("Это не трехзначное число");
+}
