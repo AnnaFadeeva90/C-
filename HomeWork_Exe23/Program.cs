10b7fbe60481f15f4a92504a30fbafa68a1a7b5e
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-16 14:53:44 +0300

    Решение 23тьей задачи

---
+++ HomeWork_Exe23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите целое положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int FirstCube = 1;
+
+while (FirstCube<=n)
+{
+    Console.Write(Math.Pow(FirstCube, 3)+ ";");
+    FirstCube++;
+}
+
