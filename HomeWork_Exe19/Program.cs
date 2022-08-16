58a3d7a5be487a70ec8fa7cff6865740bb951434
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-16 14:24:59 +0300

    Решение 19ой задачи

---
+++ HomeWork_Exe19/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначое число");
+int n = Convert.ToInt32(Console.ReadLine());
+string OurNumber = Convert.ToString(n);
+if (OurNumber[0] == OurNumber[4] || OurNumber[1]==OurNumber[3])
+{
+    Console.WriteLine("Число является полимером");
+}
+else
+{
+    Console.WriteLine("Число полимером не является");
+}
