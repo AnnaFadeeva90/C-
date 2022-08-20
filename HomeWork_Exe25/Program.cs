a8d1919210157d9d62571a3a40a3d92684790819
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-20 12:50:28 +0300

    Решение 25той задачи

---
+++ HomeWork_Exe25/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+int Number(string n)
+{
+    Console.Write($"Введите число {n}:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = Number("A");
+int B = Number("B");
+
+int pow = 1;
+for(int i=1; i<=B; i++)
+{
+    pow *=A ;
+}
+Console.WriteLine($"{A} ^ {B}= {pow}");
