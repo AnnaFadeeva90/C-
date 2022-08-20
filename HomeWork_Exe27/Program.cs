64fff4eb0225a51ad16175a56b4666e3e522ce48
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-20 12:59:00 +0300

    Решение 27 задачи

---
+++ HomeWork_Exe27/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int Number(string n)
+{
+    Console.Write($"Введите число {n}:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = Number("A");
+
+int sum=0;
+while (A>0)
+{
+    int num = A % 10;
+    A= A/10;
+    sum = sum+num;
+}
+Console.WriteLine($"Сумма цифр в числе:{sum}");
