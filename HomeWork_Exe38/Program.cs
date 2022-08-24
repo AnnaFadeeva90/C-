1554601484d68c21a152762062aa4491b4b421b6
Anna Fadeeva <anutik1990@yandex.ru>
2022-08-24 14:38:08 +0300

    Решение 38 задачи

---
+++ HomeWork_Exe38/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static int MaxNumberArray(int[]array)
+    {
+        int max = 0;
+        for (int i=0;i<array.Length; i++)
+        {
+            if (array[i]> max)
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    static int MinNumberArray(int[]array)
+    {
+        int min = array[0];
+        for (int i=0;i<array.Length; i++)
+        {
+            if (array[i]< min)
+            {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    static void Main (string[] args)
+    {
+        int[] array = {1,2,4,5,56,6,2,61,67,16,1};
+        int max = MaxNumberArray(array);
+        int min = MinNumberArray(array);
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+        Console.WriteLine(max-min);
+    }
+}
